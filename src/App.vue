802f5245f1b5d784a5478256fe9fb649d27ea2b9
Kyle Stearns <kyle@leesa.com>
2017-09-22 22:51:50 -0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h3>spoofer</h3>
+    <SpooferForm></SpooferForm>
+    <SpooferList v-bind:values="data.spoofItems"></SpooferList>
+  </div>
+</template>
+
+<script>
+import 'whatwg-fetch'
+
+import SpooferForm from './components/SpooferForm'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      spoofItems: [],
+    }
+  },
+  components: {
+    SpooferForm,
+  },
+  methods: {
+    createSpoofItem(spoofItem) {
+      // bypass fetch
+      this.spoofItems.push({
+        title: spoofItem.title,
+        description: spoofItem.description,
+        image: spoofItem.image,
+        spoofId: spoofItem.spoofId,
+      })
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SpooferForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form class="SpooferForm" v-on:submit.prevent="submit">
+    <div class="SpooferForm__field">
+      <label class="SpooferForm__label">Title</label>
+      <input type="text" class="SpooferForm__input SpooferForm__input--text" v-model="title">
+    </div>
+    <div class="SpooferForm__field">
+      <label class="SpooferForm__label">Description</label>
+      <input type="text" class="SpooferForm__input SpooferForm__input--text" v-model="description">
+    </div>
+    <div class="SpooferForm__field">
+      <label class="SpooferForm__label">Image</label>
+      <input type="text" class="SpooferForm__input SpooferForm__input--text" v-model="image">
+    </div>
+
+    <button type="submit" class="SpooferForm__submit">Pop it!</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SpooferForm',
+  data() {
+    return {
+      title: '',
+      description: '',
+      image: '',
+    }
+  },
+  methods: {
+    submit() {
+      const { title, description, image } = this
+
+      this.$parent.createSpoofItem({
+        title,
+        description,
+        image,
+      })
+    },
+  },
+}
+</script>
